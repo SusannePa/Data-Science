643043ea4e77c850fbb1ffd0e1b43f3b4b90cb3d
Susanne Pålsson <precy62@live.se>
2024-12-17 14:03:59 +0100

    Spara mina filer

--- hello.py
+++ hello.py
@@ -5,8 +5,46 @@ print("Hej")
 
 print
 
-print("Fel indentering")
 
 x=10
 y=20
 print(f"summan av x och y är: {x+y}")
+
+heltal=10
+flyttal=3.14
+sanninvärde = True
+print(f"Python", sanninvärde)
+
+x="123"
+y=2
+print()
+
+x="123"
+y=2
+z=int(x)*y#
+
+""" 5 + 3-Subtraktion x = 5 - 3
+ * Multiplikation x = 5 * 3
+ / Division (flyttal) x = 5 / 2
+ // Heltalsdivision 5 // 2
+ % Modulus 5 % 2
+ ** Exponentiering 5 ** 2 """
+
+
+tal = 0,1
+tal = 0.2 #)
+
+# x = input ("5")
+
+
+x = input("Ange tal: ") 
+y = input("Ange ett till tal: ")
+summa = int(x) + int(y)
+print(f"Summan blir: {summa}")
+
+
+x = 42
+
+text = "Talet är: " + str(x)
+
+print(text)
